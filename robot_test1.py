87536369a7a6e905a7ddd21c8ea31fa052b36e5a
ericrqs <eric.r@quali.com>
2017-03-09 18:10:16 -0500

    for first tag

--- robot_test1.py
+++ robot_test1.py
@@ -1,10 +1,10 @@
 def test1_func(cloudshell_reservation_id, cloudshell_server_address, cloudshell_port, cloudshell_username, cloudshell_password, cloudshell_domain):
 
-    print('HELLO1 FROM TEST BRANCH 1')
-    print('HELLO2 FROM TEST BRANCH 1')
-    print('HELLO3 FROM TEST BRANCH 1')
-    print('HELLO4 FROM TEST BRANCH 1')
-    print('HELLO5 FROM TEST BRANCH 1')
+    print('HELLO1 FROM TEST TAG 1')
+    print('HELLO2 FROM TEST TAG 1')
+    print('HELLO3 FROM TEST TAG 1')
+    print('HELLO4 FROM TEST TAG 1')
+    print('HELLO5 FROM TEST TAG 1')
     print('CloudShell reservation id: %s' % cloudshell_reservation_id)
     print('CloudShell server address: %s' % cloudshell_server_address)
     print('CloudShell port: %d' % cloudshell_port)
