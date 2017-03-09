a3f2e892fd576c64ed97cc2f5188826ba10862b9
ericrqs <eric.r@quali.com>
2017-03-09 18:12:01 -0500

    add message identifying dev branch

--- robot_test1.py
+++ robot_test1.py
@@ -1,4 +1,5 @@
 def test1_func(cloudshell_reservation_id, cloudshell_server_address, cloudshell_port, cloudshell_username, cloudshell_password, cloudshell_domain):
+    print('hello from ericr_dev')
     print('CloudShell reservation id: %s' % cloudshell_reservation_id)
     print('CloudShell server address: %s' % cloudshell_server_address)
     print('CloudShell port: %d' % cloudshell_port)
