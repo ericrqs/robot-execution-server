22dbe3b936673ca560bcc024562829c7949d26ca
ericrqs <eric.r@quali.com>
2017-03-09 18:11:27 -0500

    another branch update

--- robot_test1.py
+++ robot_test1.py
@@ -1,10 +1,10 @@
 def test1_func(cloudshell_reservation_id, cloudshell_server_address, cloudshell_port, cloudshell_username, cloudshell_password, cloudshell_domain):
 
-    print('HELLO1 FROM TEST TAG 2')
-    print('HELLO2 FROM TEST TAG 2')
-    print('HELLO3 FROM TEST TAG 2')
-    print('HELLO4 FROM TEST TAG 2')
-    print('HELLO5 FROM TEST TAG 2')
+    print('HELLO1 FROM LATEST TEST BRANCH')
+    print('HELLO2 FROM LATEST TEST BRANCH')
+    print('HELLO3 FROM LATEST TEST BRANCH')
+    print('HELLO4 FROM LATEST TEST BRANCH')
+    print('HELLO5 FROM LATEST TEST BRANCH')
     print('CloudShell reservation id: %s' % cloudshell_reservation_id)
     print('CloudShell server address: %s' % cloudshell_server_address)
     print('CloudShell port: %d' % cloudshell_port)
