123db1c3edbc9f6c8ae8bf4ddaf2542e9d9c845c
ericrqs <eric.r@quali.com>
2017-03-10 13:59:52 -0500

    for fifth test tag

--- robot_test1.py
+++ robot_test1.py
@@ -1,5 +1,5 @@
 def test1_func(cloudshell_reservation_id, cloudshell_server_address, cloudshell_port, cloudshell_username, cloudshell_password, cloudshell_domain):
-    print('hello from ericr_dev')
+    print('hello from test tag 5')
     print('CloudShell reservation id: %s' % cloudshell_reservation_id)
     print('CloudShell server address: %s' % cloudshell_server_address)
     print('CloudShell port: %d' % int(cloudshell_port))
