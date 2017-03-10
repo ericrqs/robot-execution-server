a076d0ef20eadf463b593f1868d1d55ffbd89655
ericrqs <eric.r@quali.com>
2017-03-10 14:00:31 -0500

    for branch 3 latest

--- robot_test1.py
+++ robot_test1.py
@@ -1,5 +1,5 @@
 def test1_func(cloudshell_reservation_id, cloudshell_server_address, cloudshell_port, cloudshell_username, cloudshell_password, cloudshell_domain):
-    print('hello from test tag 6')
+    print('hello from testbr3 latest')
     print('CloudShell reservation id: %s' % cloudshell_reservation_id)
     print('CloudShell server address: %s' % cloudshell_server_address)
     print('CloudShell port: %d' % int(cloudshell_port))
