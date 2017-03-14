1d84089891ca702eed962b6be0da2dc2bff7bf84
ericrqs <eric.r@quali.com>
2017-03-13 23:24:39 -0400

    sanitize error message characters

--- cloudshell/custom_execution_server/custom_execution_server.py
+++ cloudshell/custom_execution_server/custom_execution_server.py
@@ -102,7 +102,7 @@ class ErrorCommandResult(CommandResult):
         CommandResult.__init__(self)
         self.result = 'Error'
         self.error_name = error_name
-        self.error_description = error_description
+        self.error_description = re.sub(r'[^0-9a-zA-Z":.,/ \n]', '_', error_description.replace('\t', '    '))
 
 
 class CustomExecutionServerCommandHandler:
